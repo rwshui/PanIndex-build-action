146396813dcaaf5a4df9e2a366555eaa90e1fa90
libsgh <woiyyng@gmail.com>
2022-03-01 17:39:40 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,1 @@
 ls -n
-go env -w GO111MODULE=on
-docker pull techknowlogick/xgo:latest
-go install src.techknowlogick.com/xgo@latest
-go install github.com/gobuffalo/packr/v2@latest
-sudo apt install upx
-ls -n
