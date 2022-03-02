2195d0f12d9f9556b4157d16391e9de6fc1711a2
libsgh <woiyyng@gmail.com>
2022-03-02 10:35:06 +0800

    update

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ BUILD(){
   -X 'github.com/libsgh/PanIndex/module.GIT_COMMIT_SHA=$(git show -s --format=%H)' \
   "
   packr2
-  xgo --targets=linux/arm -out PanIndex -ldflags="$ldflags" .
+  xgo --targets=linux/arm64 -out PanIndex -ldflags="$ldflags" .
   mkdir -p dist/compress
   mv PanIndex-* dist
   cd dist
@@ -22,8 +22,7 @@ RELEASE(){
   ls -n
   for i in $(find . -type f -name "PanIndex*"); do
     if [ [[ "$i" =~ "windows" ]] ]; then
-      echo $(echo $i | sed 's/\.[^.]*$//')
-      echo $i | sed 's/\.[^.]*$//'
+      echo "compress/$(echo $i | sed 's/\.[^.]*$//').zip"
       zip compress/$(echo $i | sed 's/\.[^.]*$//').zip "$i"
     else
        tar -czvf compress/"$i".tar.gz "$i"
@@ -35,6 +34,7 @@ RELEASE(){
   cp -R -f static/ ui/static/
   cp -R -f templates/ ui/templates/
   cd ui
+  ls -n
   zip -vr dist/compress/ui-${RELEASE_TAG}.zip *
   sha256sum ui-${RELEASE_TAG}.zip >> ${GITHUB_WORKSPACE}/dist/compress/sha256.list
   cd dist/compress
