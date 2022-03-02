8cb6026658fef816edd3c71f08967297856883ab
libsgh <woiyyng@gmail.com>
2022-03-02 14:05:51 +0800

    update

--- build.sh
+++ build.sh
@@ -10,36 +10,40 @@ BUILD(){
   -X 'github.com/libsgh/PanIndex/module.GIT_COMMIT_SHA=$(git show -s --format=%H)' \
   "
   packr2
-  xgo --targets=linux/arm64 -out PanIndex -ldflags="$ldflags" .
+  xgo --targets=linux/amd64,windows/* -out PanIndex -ldflags="$ldflags" .
   mkdir -p ${GITHUB_WORKSPACE}/dist/compress
   mv PanIndex-* dist
   cd dist
   upx -9 ./PanIndex-linux*
-  #upx -9 ./PanIndex-windows*
+  upx -9 ./PanIndex-windows*
 }
 
-RELEASE(){
-  ls -n
-  for i in $(find . -type f -name "PanIndex*"); do
-    if [[ "$i" =~ "windows" ]]; then
-      echo "compress/$(echo $i | sed 's/\.[^.]*$//').zip"
-      zip compress/$(echo $i | sed 's/\.[^.]*$//').zip "$i"
-    else
-       tar -czvf compress/"$i".tar.gz "$i"
-    fi
-    sha256sum "$i" >> ${GITHUB_WORKSPACE}/dist/compress/sha256.list
-  done
+COMPRESS_UI(){
   cd ${GITHUB_WORKSPACE}
   mkdir ui
   cp -R -f static/ ui/static/
   cp -R -f templates/ ui/templates/
   cd ui
   zip -vr ${GITHUB_WORKSPACE}/dist/compress/ui-${RELEASE_TAG}.zip *
-  sha256sum ${GITHUB_WORKSPACE}/dist/compress/ui-${RELEASE_TAG}.zip >> ${GITHUB_WORKSPACE}/dist/compress/sha256.list
-  cd ${GITHUB_WORKSPACE}/dist
-  pwd
+  sha256sum ui-${RELEASE_TAG}.zip >> ${GITHUB_WORKSPACE}/dist/compress/sha256.list
+}
+
+RELEASE(){
+  cp -r LICENSE README.md ${GITHUB_WORKSPACE}/dist
+  for f in $(find ${GITHUB_WORKSPACE}/dist -type f -name "PanIndex*"); do
+    echo $f
+    if [[ "$f" =~ "windows" ]]; then
+      echo "compress/$(echo $f | sed 's/\.[^.]*$//').zip"
+      zip compress/$(echo $f | sed 's/\.[^.]*$//').zip "$f" LICENSE README.md
+    else
+       tar -czvf compress/"$f".tar.gz "$f" LICENSE README.md
+    fi
+    sha256sum "$f" >> ${GITHUB_WORKSPACE}/dist/compress/sha256.list
+  done
+  COMPRESS_UI
+  ls -n ${GITHUB_WORKSPACE}/dist/compress
 }
 
 BUILD
-RELEASE
+COMPRESS_UI
 
